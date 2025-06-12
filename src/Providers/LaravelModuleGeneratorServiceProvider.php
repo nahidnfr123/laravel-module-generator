e6e07f06af05cf0c220b9a1512157fd2679c8a4b
nahid ferdous <nahidferdous240@gmail.com>
2025-06-12 16:33:30 +0600

    v2 authentication

--- src/Providers/LaravelModuleGeneratorServiceProvider.php
+++ src/Providers/LaravelModuleGeneratorServiceProvider.php
@@ -7,19 +7,21 @@ use NahidFerdous\LaravelModuleGenerator\Console\Commands\GenerateDbDiagram;
 use NahidFerdous\LaravelModuleGenerator\Console\Commands\GenerateModuleFromModelName;
 use NahidFerdous\LaravelModuleGenerator\Console\Commands\GenerateModuleFromYaml;
 use NahidFerdous\LaravelModuleGenerator\Console\Commands\GeneratePostmanCollection;
+use NahidFerdous\LaravelModuleGenerator\Console\Commands\Install;
 use NahidFerdous\LaravelModuleGenerator\Console\Commands\ModuleRollback;
 
 class LaravelModuleGeneratorServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/module-generator.php', 'module-generator');
+        $this->mergeConfigFrom(__DIR__ . '/../config/module-generator.php', 'module-generator');
     }
 
     public function boot()
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
+                Install::class,
                 GenerateModuleFromModelName::class,
                 GenerateModuleFromYaml::class,
                 GeneratePostmanCollection::class,
@@ -29,24 +31,19 @@ class LaravelModuleGeneratorServiceProvider extends ServiceProvider
 
             // Publish stubs separately
             $this->publishes([
-                __DIR__.'/../stubs' => base_path('module/stubs'),
+                __DIR__ . '/../stubs' => base_path('module/stubs'),
             ], 'module-generator-stubs');
 
             // Publish config separately
             $this->publishes([
-                __DIR__.'/../config/module-generator.php' => config_path('module-generator.php'),
+                __DIR__ . '/../config/module-generator.php' => config_path('module-generator.php'),
             ], 'module-generator-config');
 
             // Publish both together with general tag
             $this->publishes([
-                __DIR__.'/../stubs' => base_path('module/stubs'),
-                __DIR__.'/../config/module-generator.php' => config_path('module-generator.php'),
+                __DIR__ . '/../stubs' => base_path('module/stubs'),
+                __DIR__ . '/../config/module-generator.php' => config_path('module-generator.php'),
             ], 'module-generator');
-
-            // Auto-create models.yaml if not exists
-//            $this->ensureModelsYamlExists();
-//            $this->generateApiTraits();
-//            $this->generateMetaTrait();
         }
     }
 }
