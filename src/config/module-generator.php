ce6fcba7e45b5814e359d8ff2c3551f1a0b94952
Nahid Ferdous <nahid@encoderit.net>
2026-01-07 10:59:49 +0600

    _ _

--- src/config/module-generator.php
+++ src/config/module-generator.php
@@ -22,8 +22,7 @@ return [
     ],
     // Postman collection settings
     'postman' => [
-        'default_base_url' => '127.0.0.1:8000',
-        // 'default_base_url' => '{{base-url}}',
+        'default_base_url' => config('app.url'),
         'default_prefix' => 'api',
         'output_path' => "module/postman_collection_{$randomNumber}.json",
     ],
